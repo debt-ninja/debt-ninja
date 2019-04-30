1866483606fbc701fb75444c5c954114f7c9e82a
Kornek <alex-do2011@hotmail.de>
2019-04-30 15:13:47 +0200

    refactor: updated init.sql script

--- server/init.sql
+++ server/init.sql
@@ -1,7 +1,22 @@
-CREATE TABLE Persons (
-    PersonID int,
-    LastName varchar(255),
-    FirstName varchar(255),
-    Address varchar(255),
-    City varchar(255) 
+SET timezone = 'Europe/Vienna';
+
+show timezone;
+
+create table Users
+(
+    UserId   int not null,
+    UserName varchar(255),
+    primary key (UserId)
+);
+
+create table Receipts
+(
+    ReceiptId        int not null,
+    ReceiptLink      varchar(255),
+    ReceiptTimestamp timestamp,
+    ReceiptTotal     numeric(5, 2),
+    ReceiptDebt      numeric(5, 2),
+    UserId           int,
+    primary key (ReceiptId),
+    foreign key (UserId) references Users (UserId)
 );
